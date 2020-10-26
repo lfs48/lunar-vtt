217c90631a0f2bf6b84c0d93d98bebfadff155df
lucas schraier <lucasfschraier@gmail.com>
2020-10-26 12:34:00 -0400

    Define login route to log in a user

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,4 +28,22 @@ router.post('/register', (req, res) => {
 
 });
 
+router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({username})
+    .then( (user) => {
+        if (!user) { return res.status(404).json({error: "Incorrect username or password"}); }
+        bcrypt.compare(password, user.password)
+        .then( (isMatch) => {
+            if (isMatch) {
+                res.json({msg: "Login success"});
+            } else {
+                return res.status(400).json({error: "Incorrect username or password"});
+            }
+        });
+    });
+});
+
 module.exports = router;
