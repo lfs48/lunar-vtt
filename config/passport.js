4771583996b578aa84f4d77d274097ef7cb7a4e2
lucas schraier <lucasfschraier@gmail.com>
2020-10-27 14:08:00 -0400

    Remove test message, implement retrieving and returning user data on successful authentication

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,14 @@ options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
   passport.use(new JwtStrategy(options, (jwt_payload, done) => {
-    console.log(jwt_payload);
+    User.findById(jwt_payload.id)
+    .then( (user) => {
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    })
+    .catch( (err) => console.log(err));
   }));
 };
