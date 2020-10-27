1e5de176ffdb46a1ceecff5a3d613f58865fefc1
lucas schraier <lucasfschraier@gmail.com>
2020-10-27 14:08:00 -0400

    Use passport for jwt authentication

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require('passport');
 const users = require("./routes/api/users");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 const db = require('./config/keys').mongoURI;
 mongoose
