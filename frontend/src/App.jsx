390e463fafc4f45576da71e97b01eb438959114b
lucas schraier <lucasfschraier@gmail.com>
2020-11-05 13:20:00 -0500

    Replace placeholder 'hello world' render with routing Switch and '/' route to landing component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from './util/routes_util';
+import { Switch } from 'react-router-dom';
+
+import Landing from './components/landing';
+
+const App = () => (
+    <Switch>
+        <AuthRoute exact path="/" component={Landing} />
+    </Switch>
+);
 
 export default App;
