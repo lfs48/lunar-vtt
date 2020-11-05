f32b8eaa9681dba76c8cfa595a52ea9f2d6b9700
lucas schraier <lucasfschraier@gmail.com>
2020-11-05 13:20:00 -0500

    Add Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,17 @@ import { Switch } from 'react-router-dom';
 
 import Landing from './components/landing';
 import Login from './components/login';
+import Navbar from './components/navbar';
 
 const App = () => {
     return(
-    <Switch>
-        <AuthRoute exact path="/" component={Landing} />
-        <AuthRoute path="/login" component={Login} />
-    </Switch>
+      <main>
+      <Navbar />
+      <Switch>
+          <AuthRoute exact path="/" component={Landing} />
+          <AuthRoute path="/login" component={Login} />
+      </Switch>
+    </main>
     );
 };
 
