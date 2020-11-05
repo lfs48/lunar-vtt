57acb2ae78ccdde69505628abda52fab352eb19e
lucas schraier <lucasfschraier@gmail.com>
2020-11-05 13:20:00 -0500

    Add login authroute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,15 @@ import { AuthRoute, ProtectedRoute } from './util/routes_util';
 import { Switch } from 'react-router-dom';
 
 import Landing from './components/landing';
+import Login from './components/login';
 
-const App = () => (
+const App = () => {
+    return(
     <Switch>
         <AuthRoute exact path="/" component={Landing} />
+        <AuthRoute path="/login" component={Login} />
     </Switch>
-);
+    );
+};
 
 export default App;
