e5ac4bc1df2f2948921b6993ac2826ca53a7dfb6
lucas schraier <lucasfschraier@gmail.com>
2020-11-05 13:20:00 -0500

    Add login and register actions

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,11 +1,45 @@
 import * as APIUtil from '../util/api/session_api_util';
 import jwt_decode from 'jwt-decode';
 
-import {RECEIVE_USER_LOGOUT} from './types';
+import {RECEIVE_USER_LOGIN, RECEIVE_CURRENT_USER, RECEIVE_USER_LOGOUT} from './types';
+
+// Standard actions
+
+export const receiveCurrentUser = (user) => ({
+    type: RECEIVE_CURRENT_USER,
+    user
+});
+
+export const receiveUserLogin = () => ({
+    type: RECEIVE_USER_LOGIN
+});
 
 export const logoutUser = () => ({
     type: RECEIVE_USER_LOGOUT
 });
+
+// Thunk actions
+
+export const signup = (user) => (dispatch) => (
+    APIUtil.signup(user).then( () => (
+        dispatch(receiveUserSignIn())
+    ), (err) => (
+        dispatch(receiveErrors(err.response.data))
+    ))
+);
+
+export const login = (user) => (dispatch) => (
+    APIUtil.login(user).then( (res) => {
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        APIUtil.setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(receiveCurrentUser(decoded))
+    })
+    .catch( (err) => {
+        dispatch(receiveErrors(err.response.data));
+    })
+);
 
 export const logout = () => dispatch => {
     // Remove the token from local storage
