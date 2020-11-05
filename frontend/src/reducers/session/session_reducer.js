7bd6ee95184ff9681c92f0f2b48bf6dc5404c5b2
lucas schraier <lucasfschraier@gmail.com>
2020-11-05 13:20:00 -0500

    Handle login actions

--- frontend/src/reducers/session/session_reducer.js
+++ frontend/src/reducers/session/session_reducer.js
@@ -1,4 +1,5 @@
-import { RECEIVE_USER_LOGOUT } from '../../actions/types';
+import { RECEIVE_CURRENT_USER, RECEIVE_USER_LOGIN, RECEIVE_USER_LOGOUT } from '../../actions/types';
+import {merge} from 'lodash';
 
 const initialState = {
   isAuthenticated: false,
@@ -7,14 +8,26 @@ const initialState = {
 
 const sessionReducer = (state = initialState, action) => {
   switch (action.type) {
+
+    default: return state;
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser
+      };
+    case RECEIVE_USER_LOGIN:
+      return {
+        ...state,
+        isSignedIn: true
+      }
     case RECEIVE_USER_LOGOUT:
       return {
         isAuthenticated: false,
         user: undefined
       };
-    default:
-      return state;
   }
+
 };
 
 export default sessionReducer;
