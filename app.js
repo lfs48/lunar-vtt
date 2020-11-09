78bb34251c6b73a044c7bedb367d7e2ba457a19c
lucas schraier <lucasfschraier@gmail.com>
2020-11-09 12:57:00 -0500

    Load static build folder in production

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const path = require('path');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require('passport');
 const users = require("./routes/api/users");
 
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
