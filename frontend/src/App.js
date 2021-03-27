4c1d68548c14babe5d69df01135570ada4da8897
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-27 00:24:33 -0400

    Add tailwind styles to test on heroku

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <header>PLACEHOLDER</header>
+      <header className="text-2xl text-red-500">PLACEHOLDER</header>
     </div>
   );
 }
