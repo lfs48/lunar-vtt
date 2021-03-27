e44071029ddd3277f23a4330773b790c1074b528
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-27 18:24:57 -0400

    Try more route stuff

--- app.js
+++ app.js
@@ -12,18 +12,8 @@ app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  let protected = ['transformed.js', 'main.css', 'favicon.ico']
-  app.get("*", (req, res) => {
-
-    let path = req.params['0'].substring(1)
-  
-    if (protected.includes(path)) {
-      // Return the actual file
-      res.sendFile(`${__dirname}/frontend/build/${path}`);
-    } else {
-      // Otherwise, redirect to /build/index.html
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'public/index.html'));
-    }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'public/index.html'));
   });
 }
 
