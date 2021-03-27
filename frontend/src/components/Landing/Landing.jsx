124e41e2765373bf6865cb9d6242fd96c4a0877d
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-27 12:58:25 -0400

    Implement controlled inputs for username and pw, login button

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Header, Input } from '../../styles/components';
+import { apiLogin} from '../../util/api/apiAuthUtil'
+import { handleInput } from '../../util/functions/utilFunctions';
 
 export default function Landing() {
+    
+    const [inputs, setInputs] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+        apiLogin(inputs);
+    }
+
     return(
-        <h1 className="text-red-500">PLACEHOLDER!!</h1>
+        <>
+        <div className="flex h-full flex-col justify-center items-center">
+            <Header className="mb-6">Lunar VTT</Header>
+            <div className="flex flex-col w-96">
+                <Input
+                    type="text"
+                    value={inputs.username}
+                    onChange={e => handleInput(e, 'username', inputs, setInputs)}
+                ></Input>
+                <Input
+                    type="password"
+                    value={inputs.password}
+                    onChange={e => handleInput(e, 'password', inputs, setInputs)}
+                ></Input>
+                <Button onClick={e => handleLogin(e)}>Log In</Button>
+            </div>
+        </div>
+        </>
     )
 }
