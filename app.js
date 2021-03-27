0576bad51d80ba511b31f5ef932ae14b4d42091c
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-27 17:50:31 -0400

    Add static.json config file for heroku routing

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('/register', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'public/index.html'));
   });
 }
