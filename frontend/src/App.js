bda3f321c5c8c685092410af845c4fd2759e28e6
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-27 10:50:53 -0400

    Render helmet, router component, and landing page route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
-    <div>
-      <header className="text-2xl text-red-500">PLACEHOLDER</header>
-    </div>
+    <BrowserRouter>
+      <Helmet
+        titleTemplate="%s"
+        defaultTitle="Lunar VTT"
+      >
+      </Helmet>
+      <Switch>
+        <Route path="/" component={Landing} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
