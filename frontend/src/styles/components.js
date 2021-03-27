2d23d2628f1fa0184c2bd0a8fec82d6918ac6f8f
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-27 12:56:41 -0400

    Add Header, Input, Button styled components

---
+++ frontend/src/styles/components.js
@@ -0,0 +1,18 @@
+import tw from 'tailwind-styled-components';
+
+export const Header = tw.header`
+    text-2xl
+    font-bold
+`
+
+export const Input = tw.input`
+    bg-gray-100
+    rounded
+    px-2
+    py-1
+`
+
+export const Button = tw.button`
+    rounded
+    p-4
+`
