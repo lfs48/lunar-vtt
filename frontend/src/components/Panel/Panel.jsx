f5ce8f5f86c8b60acf088c51ae8cff3a7ec66bb1
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-28 22:08:02 -0400

    Implement panel resizing

--- frontend/src/components/Panel/Panel.jsx
+++ frontend/src/components/Panel/Panel.jsx
@@ -73,9 +73,49 @@ export default function Panel({data}) {
         newState.dragging = false;
         setStyleData(newState);
     };
+
+    const resize = (event, dirs) => {
+        event.preventDefault();
+        const newState = merge({}, styleData);
+
+        if(event.pageX > 0 && event.pageX < window.innerWidth - 10) {
+            if ("right" in dirs) {
+                const newWidth = event.pageX - newState.left
+                newState.width =  Math.max( newWidth, newState.minWidth );
+            } else if ("left" in dirs) {
+                const newWidth = newState.width + newState.left - event.pageX;
+                newState.width = Math.max(newWidth, newState.minWidth);
+                if (newState.width != styleData.width) {newState.left = event.pageX; }
+            }
+        }
+
+        if(event.pageY > 0) {
+            if ("top" in dirs) {
+                const newHeight = newState.height + newState.top - event.pageY;
+                newState.height = Math.max(newHeight, newState.minHeight);
+                if (newState.height != styleData.height) {newState.top = event.pageY};
+            } else if ("bottom" in dirs) {
+                const newHeight = event.pageY - newState.top;
+                newState.height = Math.max(newHeight, newState.minHeight);
+            }
+        }
+
+        setStyleData(newState);
+    }
     
     return(
-        <article draggable="true" className={`absolute cursor-move border border-black bg-white z-50 ${ styleData.stage < 2 ? "transition-all duration-700 ease-in-out" : ""}`} style={styleData}>
+        <article draggable="true" className={`absolute cursor-move border border-black bg-white shadow-xl z-50 ${ styleData.stage < 2 ? "transition-all duration-700 ease-in-out" : ""}`} style={styleData}>
+
+            <div className="resize-areas-container">
+                <div draggable="true" className="resize-area resize-top" onDrag={ e => resize(e, {top: true} ) } ></div>
+                <div draggable="true" className="resize-area resize-left" onDrag={ e => resize(e, {left: true} ) }></div>
+                <div draggable="true" className="resize-area resize-bottom" onDrag={ e => resize(e, {bottom: true} ) } ></div>
+                <div draggable="true" className="resize-area resize-right" onDrag={ e => resize(e, {right: true} ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-bottomright" onDrag={ e => resize(e, {bottom: true, right: true} ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-bottomleft" onDrag={ e => resize(e, {bottom: true, left: true} ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-topright" onDrag={ e => resize(e, {top: true, right: true} ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-topleft" onDrag={ e => resize(e, {top: true, left: true} ) }></div>
+            </div>
 
             <header draggable="true" onDrag={e => handleDrag(e)} 
             onDragEnd={e => handleDragEnd(e)}>
