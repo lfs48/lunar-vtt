f54db022634e9cca8db7f988c8875a807ab72a9b
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-30 00:21:25 -0400

    Add intToOrd and getLevelProf util functions

--- frontend/src/util/functions/utilFunctions.js
+++ frontend/src/util/functions/utilFunctions.js
@@ -10,3 +10,30 @@ export const handleInput = (event, field, state, setState, value=null) => {
   }
   setState(newState);
 };
+
+  
+export const intToOrdinal = (n) => {
+  if (n === 1 || n === '1') {
+      return '1st'
+  } else if (n === 2 || n === '2') {
+      return '2nd'
+  } else if (n === 3 || n === '3') {
+      return '3rd'
+  } else {
+      return n.toString()+'th'
+  }
+};
+
+export const getLevelProf = (level) => {
+  if (level < 5) {
+    return 2;
+  } else if (level >= 5 && level < 9) {
+    return 3;
+  } else if (level >= 9 && level < 13) {
+    return 4;
+  } else if (level >= 13 && level < 17) {
+    return 5;
+  } else if (level >= 17) {
+    return 6;
+  }
+}
