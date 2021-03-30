a5a6527c20467bea61cc4342db1e14698233aae0
Lucas Schraier <lucasfschraier@gmail.com>
2021-03-29 23:32:56 -0400

    Throttle resize

--- frontend/src/components/Panel/Panel.jsx
+++ frontend/src/components/Panel/Panel.jsx
@@ -54,13 +54,44 @@ const handleDragEnd = (event, styleData, setStyleData) => {
     setStyleData(newState);
 };
 
+const _resize = (event, dirs, styleData, setStyleData) => {
+    event.preventDefault();
+    const newState = merge({}, styleData);
+
+    if(event.pageX > 0 && event.pageX < window.innerWidth - 1) {
+        if ("right" in dirs) {
+            const newWidth = event.pageX - newState.left
+            newState.width =  Math.max( newWidth, newState.minWidth );
+        } else if ("left" in dirs) {
+            const newWidth = newState.width + newState.left - event.pageX;
+            newState.width = Math.max(newWidth, newState.minWidth);
+            if (newState.width != styleData.width) {newState.left = event.pageX; }
+        }
+    }
+
+    if(event.pageY > 0 && event.pageY < window.innerHeight - 1) {
+        if ("top" in dirs) {
+            const newHeight = newState.height + newState.top - event.pageY;
+            newState.height = Math.max(newHeight, newState.minHeight);
+            if (newState.height != styleData.height) {newState.top = event.pageY};
+        } else if ("bottom" in dirs) {
+            const newHeight = event.pageY - newState.top;
+            newState.height = Math.max(newHeight, newState.minHeight);
+        }
+    }
+
+    setStyleData(newState);
+};
+
+const resize = throttle(_resize, 20);
+
 export default function Panel({data, panelType}) {
 
     const dispatch = useDispatch();
 
     const [styleData, setStyleData] = useState({
-        left: Math.max( Math.random() * window.innerWidth - 500, 0),
-        top: Math.max( Math.random() * window.innerHeight - 400, 0),
+        left: Math.max( Math.random() * window.innerWidth - 800, 0),
+        top: Math.max( Math.random() * window.innerHeight - 600, 0),
         width: 800,
         height: 600,
         minHeight: 50,
@@ -83,35 +114,6 @@ export default function Panel({data, panelType}) {
             setStyleData(newerState);
         }, 720);
     }, []);
-
-    const resize = (event, dirs) => {
-        event.preventDefault();
-        const newState = merge({}, styleData);
-
-        if(event.pageX > 0 && event.pageX < window.innerWidth - 1) {
-            if ("right" in dirs) {
-                const newWidth = event.pageX - newState.left
-                newState.width =  Math.max( newWidth, newState.minWidth );
-            } else if ("left" in dirs) {
-                const newWidth = newState.width + newState.left - event.pageX;
-                newState.width = Math.max(newWidth, newState.minWidth);
-                if (newState.width != styleData.width) {newState.left = event.pageX; }
-            }
-        }
-
-        if(event.pageY > 0 && event.pageY < window.innerHeight - 1) {
-            if ("top" in dirs) {
-                const newHeight = newState.height + newState.top - event.pageY;
-                newState.height = Math.max(newHeight, newState.minHeight);
-                if (newState.height != styleData.height) {newState.top = event.pageY};
-            } else if ("bottom" in dirs) {
-                const newHeight = event.pageY - newState.top;
-                newState.height = Math.max(newHeight, newState.minHeight);
-            }
-        }
-
-        setStyleData(newState);
-    };
 
     const handleClose = (event) => {
         event.preventDefault();
@@ -143,14 +145,14 @@ export default function Panel({data, panelType}) {
         <article draggable="true" className={`${panelClass} ${ styleData.stage < 2 ? "transition-all duration-700 ease-in-out" : ""} `} style={styleData}>
 
             <div className="resize-areas-container">
-                <div draggable="true" className="resize-area resize-top" onDrag={ e => resize(e, {top: true} ) } ></div>
-                <div draggable="true" className="resize-area resize-left" onDrag={ e => resize(e, {left: true} ) }></div>
-                <div draggable="true" className="resize-area resize-bottom" onDrag={ e => resize(e, {bottom: true} ) } ></div>
-                <div draggable="true" className="resize-area resize-right" onDrag={ e => resize(e, {right: true} ) }></div>
-                <div draggable="true" className="resize-area resize-corner resize-bottomright" onDrag={ e => resize(e, {bottom: true, right: true} ) }></div>
-                <div draggable="true" className="resize-area resize-corner resize-bottomleft" onDrag={ e => resize(e, {bottom: true, left: true} ) }></div>
-                <div draggable="true" className="resize-area resize-corner resize-topright" onDrag={ e => resize(e, {top: true, right: true} ) }></div>
-                <div draggable="true" className="resize-area resize-corner resize-topleft" onDrag={ e => resize(e, {top: true, left: true} ) }></div>
+                <div draggable="true" className="resize-area resize-top" onDrag={ e => resize(e, {top: true}, styleData, setStyleData ) } ></div>
+                <div draggable="true" className="resize-area resize-left" onDrag={ e => resize(e, {left: true}, styleData, setStyleData ) }></div>
+                <div draggable="true" className="resize-area resize-bottom" onDrag={ e => resize(e, {bottom: true}, styleData, setStyleData ) } ></div>
+                <div draggable="true" className="resize-area resize-right" onDrag={ e => resize(e, {right: true}, styleData, setStyleData ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-bottomright" onDrag={ e => resize(e, {bottom: true, right: true}, styleData, setStyleData ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-bottomleft" onDrag={ e => resize(e, {bottom: true, left: true}, styleData, setStyleData ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-topright" onDrag={ e => resize(e, {top: true, right: true}, styleData, setStyleData ) }></div>
+                <div draggable="true" className="resize-area resize-corner resize-topleft" onDrag={ e => resize(e, {top: true, left: true}, styleData, setStyleData ) }></div>
 
                 <PanelHeaderContainer draggable="true" onDrag={e => handleDrag(e, styleData, setStyleData)} onDragEnd={e => handleDragEnd(e, styleData, setStyleData)}>
                     <PanelHeader>{data.name}</PanelHeader>
