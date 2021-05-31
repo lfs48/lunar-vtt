ef49c8fd8b116c2241d3a9fbecef34dad6fe2e08
Lucas <lucasfschraier@gmail.com>
2021-05-31 12:55:16 -0400

    Implement feature form

---
+++ frontend/src/components/Entities/Features/FeatureForm.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createFeature, editFeature } from '../../../store/reducers/entities/featuresReducer';
+import { ClearInput, Input, TextArea } from '../../../styles/components';
+import { handleInput } from '../../../util/functions/utilFunctions';
+import ModalFooter from '../../Modal/ModalFooter';
+import Select from '../../Util/Select';
+import { FormContainer, Field, Label } from '../styles';
+import SourceSection from './SourcesSection';
+
+const featureTypeOptions = ['Action', 'Bonus Action', 'Reaction', 'Passive', 'Triggered', 'Other'];
+const sourceTypeOptions = ['DndClass', 'Subclass', 'Race', 'Background', 'Feat'];
+
+export default function FeatureForm({feature=null, edit=false}) {
+
+    const dispatch = useDispatch();
+
+    const [inputs, setInputs] = useState({
+        name: "",
+        description: "",
+        featureType: "Action",
+        sourceModel: "DndClass",
+        sourceName: "",
+        sources: []
+    });
+
+    useEffect( () => {
+        if (edit) {
+            setInputs(feature);
+        }
+    }, []);
+
+    const handleSave = () => {
+        const action = {
+            type: edit ? editFeature.type: createFeature.type,
+            payload: {
+                id: feature?._id,
+                formData: inputs
+            }
+        };
+        dispatch(action);
+    }
+
+    return(
+        <FormContainer className="w-[60rem] h-2/3">
+            <div className="p-6 grid grid-cols-4 h-[calc(100%-3rem)] gap-x-4">
+                <Field className="col-span-2">
+                    <Label>Name </Label>
+                    <Input
+                        type="text"
+                        value={inputs.name}
+                        onChange={e => handleInput(e, 'name', inputs, setInputs)}
+                    ></Input>
+                </Field>
+                <Field>
+                    <Label>Type </Label>
+                    <Select 
+                        optionList={featureTypeOptions}
+                        field={'featureType'}
+                        state={inputs}
+                        setState={setInputs}
+                    />
+                </Field>
+                <Field>
+                    <Label>Source </Label>
+                    <Select 
+                        optionList={sourceTypeOptions}
+                        field={'sourceModel'}
+                        state={inputs}
+                        setState={setInputs}
+                    />
+                </Field>
+                <Field className="flex flex-col col-span-4 row-span-6">
+                    <Label>Description </Label>
+                    <TextArea
+                        type="text"
+                        value={inputs.description}
+                        onChange={e => handleInput(e, 'description', inputs, setInputs)}
+                        className="h-full"
+                    ></TextArea>
+                </Field>
+            </div>
+            <ModalFooter
+                handleSave={() => handleSave()}
+            />
+        </FormContainer>
+    )
+}
---
+++ frontend/src/components/Entities/Features/SourcesSection.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { Button, Label } from '../../../styles/components';
+import { merge } from 'lodash';
+import EntityAutocomplete from '../../EntityAutocomplete/EntityAutocomplete';
+import { sourceModelToEntityType } from '../../../util/types/entityTypes';
+import { handleInput } from '../../../util/functions/utilFunctions';
+import { useSelector } from 'react-redux';
+
+export default function SourceSection({inputs, setInputs}) {
+
+    const [sourceInput, setSourceInput] = useState({
+        name: "",
+        id: ""
+    });
+    const [addingSource, setAddingSource] = useState(false);
+
+    const {entities} = useSelector( (state) => ({
+        entities: state.entities[sourceModelToEntityType(inputs.sourceModel)]
+    }));
+
+    const saveSource = (event) => {
+        event.preventDefault();
+        const newState = merge({}, inputs);
+        if (!newState.sources.includes(sourceInput.id)) {
+            newState.sources.push(sourceInput.id);
+        }
+        setInputs(newState);
+        setSourceInput({
+            name: "",
+            id: ""
+        });
+        setAddingSource(false);
+    };
+
+    const cancelAddingSource = (event) => {
+        event.preventDefault();
+        setSourceInput({
+            name: "",
+            id: ""
+        });
+        setAddingSource(false);
+    }
+
+    const removeSource = (index) => {
+        const newState = merge({}, inputs);
+        newState.sources = newState.sources.filter( (_, i) => i !== index);
+        setInputs(newState);
+    }
+
+    const handleSelectSource = (source) => {
+        setSourceInput({
+            name: source.name,
+            id: source._id
+        });
+    }
+
+    return(
+        <div className="flex flex-col col-span-2 row-span-4">
+            <Label>Sources </Label>
+            <div className="ml-8">
+                {inputs.sources.length > 0 ?
+                    <ul className="list-disc divide-y-[1px]">
+                        {inputs.sources.map( (id, i) => {
+                            const source = entities[id];
+                            return (
+                                <li className="w-full py-0.5" key={i}>
+                                    <div className="w-full flex justify-between">
+                                        <span>{source.name}</span>
+                                        <Button onClick={() => removeSource(i)}>
+                                            <i className="text-red-500 fas fa-times"></i>
+                                        </Button>
+                                    </div>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                :
+                    <p>None</p>
+                }
+                {addingSource ? 
+                    <div className="flex items-center">
+                        <EntityAutocomplete
+                            entityType={sourceModelToEntityType(inputs.sourceModel)}
+                            input={sourceInput.name}
+                            handleInput={e => handleInput(e, 'name', sourceInput, setSourceInput)}
+                            handleSelect={(source) => handleSelectSource(source)}
+                            className="w-full"
+                        ></EntityAutocomplete>
+                        <div className="flex flex-col">
+                            <Button onClick={(e) => saveSource(e)}>
+                                <i className=" text-green-300 fas fa-check"></i>
+                            </Button>
+                            <Button onClick={(e) => cancelAddingSource(e)}>
+                                <i className="text-red-500 fas fa-times"></i>
+                            </Button>
+                        </div>
+                    </div>
+                :
+                    <Button onClick={() => setAddingSource(true)}>Add Source</Button>
+                }
+            </div>
+        </div>
+    )
+}
