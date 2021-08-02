f79ed01dd79138b7d0bcb98570606457ecc58944
Lucas <lucasfschraier@gmail.com>
2021-08-01 21:32:45 -0400

    Adjust subclass tab styling

--- frontend/src/components/Sidebar/Tabs/SubclassesTab.jsx
+++ frontend/src/components/Sidebar/Tabs/SubclassesTab.jsx
@@ -125,7 +125,7 @@ export default function SubclassesTab() {
                     </Button>
                 :<></>}
             </TabHeader>
-            <div className="overflow-y-scroll h-[calc(100vh-7rem)]">
+            <div className="overflow-y-scroll h-[calc(100vh-6rem)]">
                 {sections}
             </div>
         </div>
